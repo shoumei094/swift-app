8f7d2c92ab717fe786100d169b4721060511c6b8
Shoumei Yamamoto <cygnus.y13@gmail.com>
2017-11-30 00:31:43 +0900

    Test

--- swift-app/application/view/SearchPhotoViewController.swift
+++ swift-app/application/view/SearchPhotoViewController.swift
@@ -10,26 +10,37 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-class SearchPhotoViewController: BaseViewController {
+class SearchPhotoViewController: BaseViewController, UITableViewDataSource, UITableViewDelegate {
     // outlet
     @IBOutlet weak var tableView: UITableView!
     
     // private
     private let searchBar = UISearchBar()
     private let disposeBag = DisposeBag()
+    private let initialAlbumId = "1"
+    private let viewModel = SearchPhotoViewModel()
+    private var firstPhoto: SearchPhotoEntity?
+    private var photo: [SearchPhotoEntity] = []
+    private enum Section: Int {
+        case header = 0
+        case list
+        case count
+    }
     
     // MARK: UIViewController
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // configure search bar
+        tableView.dataSource = self
+        tableView.delegate = self
         searchBar.showsCancelButton = true
         searchBar.keyboardAppearance = .dark
         searchBar.keyboardType = .asciiCapableNumberPad
         searchBar.tintColor = .gray
         searchBar.barTintColor = .white
         searchBar.placeholder = R.string.localizable.searchBarPlaceholder()
+        navigationItem.titleView = searchBar
         
         // dismiss keyboard when the cancel button is clicked
         searchBar.rx.cancelButtonClicked
@@ -41,48 +52,32 @@ class SearchPhotoViewController: BaseViewController {
             )
             .disposed(by: disposeBag)
         
-        // load search results when text is entered in the search bar
-        searchBar.rx.text.orEmpty
-            .asDriver()
-            .throttle(1)
-            .distinctUntilChanged()
-            .flatMapLatest { (albumId) -> Driver<[SearchPhotoEntity]> in
-                guard let albumId = Int(albumId.trimmingCharacters(in: .whitespaces)) else {
-                    return Driver.just([])
+        // API calls
+        let firstPhotoObservable = viewModel.searchFirstPhoto(albumId: initialAlbumId)
+        let photoObservable = viewModel.searchPhoto(albumId: initialAlbumId)
+        let searchObservable = searchBar.rx.searchButtonClicked
+            .flatMapLatest { [weak self] _ -> Observable<[SearchPhotoEntity]> in
+                guard let `self` = self else {
+                    return Observable.empty()
                 }
-                
-                return SearchPhotoViewModel.searchPhoto(albumId: albumId)
-                    .asDriver(
-                        onErrorRecover: { [weak self] (error) in
-                            self?.handleError(error: error)
-                            return Driver.just([])
-                        }
-                    )
-                    .do(
-                        onCompleted: { [weak self] in
-                            self?.tableView.setContentOffset(.zero, animated: false)
-                        }
-                    )
+                return self.viewModel.searchPhoto(albumId: self.searchBar.text)
             }
-            .drive(tableView.rx.items(cellIdentifier: R.reuseIdentifier.searchPhotoCell.identifier, cellType: SearchPhotoCell.self)) { (_, item, cell) in
-                cell.setCell(data: item)
-            }
-            .disposed(by: disposeBag)
         
-        // place search bar inside of the navigation bar
-        navigationItem.titleView = searchBar
-        
-        // push photo article view controller
-        tableView.rx.modelSelected(SearchPhotoEntity.self)
-            .asDriver()
-            .drive(
-                onNext: { [weak self] entity in
-                    guard let strongSelf = self, let viewController = R.storyboard.photoArticle.instantiateInitialViewController() else {
+        // populate table view
+        Observable.combineLatest(firstPhotoObservable, Observable.merge(photoObservable, searchObservable)) { ($0, $1) }
+            .filter { $0.0.map { _ in true } ?? false && !$0.1.isEmpty }
+            .observeOn(MainScheduler.instance)
+            .subscribe(
+                onNext: { [weak self] (firstPhoto, photo) in
+                    guard let `self` = self else {
                         return
                     }
-                    
-                    viewController.id = entity.id
-                    strongSelf.navigationController?.pushViewController(viewController, animated: true)
+                    self.firstPhoto = firstPhoto
+                    self.photo = photo
+                    self.tableView.reloadData()
+                },
+                onError: { [weak self] error in
+                    self?.handleError(error: error)
                 }
             )
             .disposed(by: disposeBag)
@@ -108,6 +103,56 @@ class SearchPhotoViewController: BaseViewController {
         
         searchBar.resignFirstResponder()
         searchBar.isUserInteractionEnabled = false
+    }
+    
+    // MARK: UITableViewDataSource
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return Section.count.rawValue
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case Section.header.rawValue:
+            return firstPhoto.map { _ in 1 } ?? 0
+        case Section.list.rawValue:
+            return photo.count
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.section {
+        case Section.header.rawValue:
+            if let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.searchPhotoCell.identifier) as? SearchPhotoCell, let entity = firstPhoto {
+                cell.setCell(data: entity)
+                return cell
+            }
+            return UITableViewCell()
+        case Section.list.rawValue:
+            if let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.searchPhotoCell.identifier) as? SearchPhotoCell  {
+                cell.setCell(data: photo[indexPath.row])
+                return cell
+            }
+            return UITableViewCell()
+        default:
+            return UITableViewCell()
+        }
+    }
+    
+    // MARK: UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.section {
+        case Section.list.rawValue:
+            if let viewController = R.storyboard.photoArticle.instantiateInitialViewController() {
+                viewController.id = photo[indexPath.row].id
+                navigationController?.pushViewController(viewController, animated: true)
+            }
+        default:
+            return
+        }
     }
     
     // MARK: BaseViewController
